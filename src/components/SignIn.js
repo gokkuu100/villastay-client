ff127b3a710efd0aaf10b5a237a10104612b5116
gokkuu100 <wkurts247@gmail.com>
2024-01-09 16:25:12 +0300

    signin and signup components

---
+++ src/components/SignIn.js
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+function SignIn() {
+    const [formData, setFormData] = useState({ email: '', password: '' });
+    const navigate = useNavigate();
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+    };
+
+    const handleSignIn = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await fetch('http://localhost:5000/api/villas/signin', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData),
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                if (data.token) {
+                    localStorage.setItem('token', data.token);
+                    localStorage.setItem('id', data.id);  
+                    localStorage.setItem('role', data.role);
+
+                    console.log(data.token, data.role, data.id);
+
+                    if (data.role === 'guest' || data.role === 'admin') {
+                        navigate('/');
+                    } else {
+                        console.error('Invalid role');
+                    }
+                } else {
+                    console.error('Authentication failed');
+                }
+            } else {
+                console.error('Invalid credentials');
+            }
+        } catch (error) {
+            alert(`An error occurred: ${error}`);
+        }
+    };
+
+    return (
+        <div>
+            <h1>Sign In</h1>
+            <form onSubmit={handleSignIn}>
+                <label>Email:</label><br />
+                <input
+                    type='email'
+                    name='email'
+                    placeholder='Email'
+                    value={formData.email}
+                    onChange={handleInputChange}
+                    required
+                />
+                <br />
+                <label>Password:</label><br />
+                <input
+                    type='password'
+                    name='password'
+                    placeholder='Password'
+                    value={formData.password}
+                    onChange={handleInputChange}
+                    required
+                />
+                <br />
+                <button type='submit'>Sign In</button>
+            </form>
+        </div>
+    );
+}
+
+export default SignIn;
